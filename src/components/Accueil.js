5ba1dc5f2ebf2e5d9720b645403bf8ef52f06092
taharakroukii <2044661@cegeplimoilou.ca>
2023-09-14 13:10:55 -0400

    changer Accueil en une fonction
    changer Accueil en une fonction et ajouter les nodes modules et le router dom

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -1,10 +1,12 @@
-function About() {
-    return (
-        <div>
-            <h1>Accueil</h1>
-            <p>Bienvenue dans votre TP1 !!!</p>
-        </div>
-    );
+import React, { Component } from 'react';
+class About extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Accueil</h1>
+                <p>Bienvenue dans votre TP1 !!!</p>
+            </div>
+        );
+    }
 }
-
 export default About;
