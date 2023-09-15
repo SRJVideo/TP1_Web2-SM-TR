f8272fb422a21dcd2c099a79a25899b431a912bf
MabJo <1931631@cegeplimoilou.ca>
2023-09-15 12:57:30 -0400

    Affichages des valeurs dans le Tableau.js
    Création d'une liste d'éléments dans un state
    Application des valeurs d'une liste d'éléments dans le tableau.

--- src/components/Tableau.js
+++ src/components/Tableau.js
@@ -1,26 +1,44 @@
 import React, {Component} from 'react';
 import {Table} from "react-bootstrap";
 import Junior from "./sous-components/TableauJunior"
+// site déployé : https://tp1-web2-sm-tr.vercel.app/
+class Tableau extends Component {
 
-function Tableau () {
-
+    state = {
+        listElements : [
+            {modele: "Skidoo", marque:"Summit", prix: 14799},
+            {modele: "Skidoo", marque:"Freeride", prix: 19699},
+            {modele: "Skidoo", marque:"Freeride", prix:  11849},
+            {modele: "Skidoo", marque:"Expedition", prix: 11899},
+            {modele: "Skidoo", marque:"Expedition", prix: 12349},
+            {modele: "Skidoo", marque:"Expedition", prix:  10949},
+        ]
+    }
+    render() {
         return (
             <div>
                 <h1>Tableau d'évènements</h1>
                 <Table striped bordered hover>
-                   <thead>
-                       <tr>
-                           <th>Modele</th>
-                           <th>Marque</th>
-                           <th>Prix</th>
-                       </tr>
-                   </thead>
+                    <thead>
+                    <tr>
+                        <th>Modele</th>
+                        <th>Marque</th>
+                        <th>Prix</th>
+                    </tr>
+                    </thead>
                     <tbody>
-                       <Junior modele="Playstation 5" marque="Sony" prix={500}/>
+                    {this.state.listElements.map((elem, index) => {
+                       return(
+                           <div key={index}>
+                               <Junior modele={elem.modele} marque={elem.marque} prix={ new Intl.NumberFormat('fr-FR').format(elem.prix)  }/>
+                           </div>
+                       )
+                    })}
                     </tbody>
                 </Table>
             </div>
         );
+    }
 
 }
 
--- src/components/sous-components/TableauJunior.js
+++ src/components/sous-components/TableauJunior.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 function TableauJunior (props) {
     const modele = props.modele;
     const marque = props.marque;
-    const prix = Number(props.prix);
+    const prix = props.prix;
 
     return (
         <div>
