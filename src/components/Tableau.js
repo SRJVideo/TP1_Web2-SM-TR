c3ed1b25c9655ebfa077d52331132336b7b37390
MabJo <1931631@cegeplimoilou.ca>
2023-09-15 11:24:43 -0400

    Update Tableau.js
    Affichage du tableau de base (sans les éléments)

--- src/components/Tableau.js
+++ src/components/Tableau.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
+import {Table} from "react-bootstrap";
 
-class Tableau extends Component {
-    render() {
+function Tableau () {
+
         return (
             <div>
-                
+                <h1>Tableau d'évènements</h1>
+                <Table striped bordered hover>
+                   <thead>
+                       <tr>
+                           <th>Modele</th>
+                           <th>Marque</th>
+                           <th>Prix</th>
+                       </tr>
+                   </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td> $</td>
+                        </tr>
+                    </tbody>
+                </Table>
             </div>
         );
-    }
+
 }
 
 export default Tableau;
