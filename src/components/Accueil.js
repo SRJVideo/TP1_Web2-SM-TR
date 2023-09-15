168585feb395cd7a1a5764fcc9cae06ff6c61ec5
taharakroukii <2044661@cegeplimoilou.ca>
2023-09-15 11:23:54 -0400

    Changer Accueil en fonction

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
-class About extends Component{
-    render(){
-        return(
-            <div>
-                <h1>Accueil</h1>
-                <p>Bienvenue dans votre TP1 !!!</p>
-            </div>
-        );
-    }
+function About() {
+    return (
+        <div>
+            <h1>Accueil</h1>
+            <p>Bienvenue dans votre TP1 !!!</p>
+        </div>
+    );
 }
+
 export default About;
