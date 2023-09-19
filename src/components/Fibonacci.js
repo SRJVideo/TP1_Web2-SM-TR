062b8a62b27890c9d8f20515d746d75f1fb41cfc
taharakroukii <2044661@cegeplimoilou.ca>
2023-09-19 14:29:41 -0400

    Ajouter le bouton Calcul et le 2eme label dans Fibonacci

--- src/components/Fibonacci.js
+++ src/components/Fibonacci.js
@@ -4,7 +4,12 @@ class Fibonacci extends Component {
    state = {
         valeur_entree: 0,
         valeur_finale: 0
+    }
 
+    handleValeur_entree = (e) => {
+        this.setState({
+            valeur_entree: e.target.value
+        })
     }
 
   
@@ -12,7 +17,15 @@ class Fibonacci extends Component {
     render() {
         return (
             <div>
-                <h1>Calculateur de Fibonacci</h1>                
+                <h1>Calculateur de Fibonacci</h1>
+                <label>
+                    Enter n:
+                    <input type="number" value={this.state.valeur_entree} onChange={this.handleValeur_entree} />
+                </label> 
+                <button onClick>Calcul</button><br/>
+                <label>
+                    Fibonacci() =
+                </label>              
             </div>
         );
     }
