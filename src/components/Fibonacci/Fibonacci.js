8c59af8bb76863ea2ffb077453912af416af6bec
taharakroukii <2044661@cegeplimoilou.ca>
2023-09-25 20:31:16 -0400

    Terminer Carousel

--- src/components/Fibonacci/Fibonacci.js
+++ src/components/Fibonacci/Fibonacci.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import  {calculFibonacci}  from './CalculFibonacci.js';
+/*import  {calculFibonacci}  from './CalculFibonacci.js';*/
 
 
 class Fibonacci extends Component {
@@ -10,24 +10,41 @@ class Fibonacci extends Component {
 
     handleValeur_entree = (e) => {
         this.setState({
-            valeur_entree: e.target.value
+            valeur_entree: Number(e.target.value)
         })
+    }
+
+   
+
+     calculFibonacci = (num) => {
+        if (num === 0) {
+            return 0
+        }
+        else if (num === 1) {
+            return 1
+        }
+        else {
+            return this.calculFibonacci(num - 1) + this.calculFibonacci(num - 2)
+        }
     }
 
     handleValeur_finale = (e) => {
         this.setState({
-            valeur_finale: e.target.value
+            valeur_finale: Number(e.target.value),
         })
+        this.valeur_finale = this.calculFibonacci(this.state.valeur_entree)
+
+
     }
 
     //faire une fonction qui appelle calculFibonacci puis met a jour le state 
 
 
-    appelCalculFibonacci = () => {
+    /*appelCalculFibonacci = () => {
         this.setState({
             valeur_finale: calculFibonacci(this.state.valeur_entree)
         })
-    }
+    }*/
 
 
 
@@ -41,8 +58,8 @@ class Fibonacci extends Component {
                     Enter n:
                     <input type="number" onChange={this.handleValeur_entree} />
                 </label>
-                <button onClick={() => this.appelCalculFibonacci()}>Calcul</button><br />
-                Fibonacci({this.state.valeur_finale}) :{this.state.valeur_finale}
+                <button onClick={() => this.handleValeur_finale}>Calcul</button><br />
+                Fibonacci({this.state.valeur_entree}) ={this.state.valeur_finale}
             </div>
         );
     }
--- src/components/MonCarousel.js
+++ src/components/MonCarousel.js
@@ -1,9 +1,9 @@
 import Carousel from 'react-bootstrap/Carousel';
-import Monimage1 from '../images/messi.jpg';
-import Monimage2 from '../images/maradona.jpg';
-import Monimage3 from '../images/pele.jpg';
-import Monimage4 from '../images/ronaldo.jpg';
-import Monimage5 from '../images/zidane.jpg';
+import Monimage1 from '../images/aigle.jpg';
+import Monimage2 from '../images/anaconda.jpg';
+import Monimage3 from '../images/leopard.jpg';
+import Monimage4 from '../images/lion.webp';
+import Monimage5 from '../images/tigre.webp';
 
 
 function MonCarousel() {
@@ -13,44 +13,59 @@ function MonCarousel() {
       <h1 style={{ textAlign: "left" }}>Carousel</h1>
       <Carousel>
         <Carousel.Item>
-        <Monimage1 text="First slide" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Messi gagne la coupe du monde.</p>
-        </Carousel.Caption>
-         
+          <img
+            className="d-block w-100"
+            src={Monimage1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Aigle</h3>
+            <p>Le roi du ciel</p>
+          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-
-        <Monimage2 text="Second  slide" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Maradona gagne la coupe du monde.</p>
-        </Carousel.Caption>
+          <img
+            className="d-block w-100"
+            src={Monimage2}
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Anaconda</h3>
+            <p>Le plus puissant</p>
+          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-
-        <Monimage3 text="Third  slide" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Pele gagne la coupe du monde.</p>
-        </Carousel.Caption>
+          <img
+            className="d-block w-100"
+            src={Monimage3}
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Léopard</h3>
+            <p>Léopard sur la branche d'arbre</p>
+          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-
-        <Monimage4 text="Fourth  slide" />
-        <Carousel.Caption>
-          <h3>Fourth slide label</h3>
-          <p>Ronaldo gagne la coupe du monde.</p>
-        </Carousel.Caption>
+          <img
+            className="d-block w-100"
+            src={Monimage4}
+            alt="Fourth slide"
+          />
+          <Carousel.Caption>
+            <h3>Lion</h3>
+            <p>Le roi de la jungle</p>
+          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-
-        <Monimage5 text="Fifth slide" />
-        <Carousel.Caption>
-          <h3>Fifth slide label</h3>
-          <p>Zidane gagne la coupe du monde.</p>
-        </Carousel.Caption>
+          <img
+            className="d-block w-100"
+            src={Monimage5}
+            alt="Fifth slide"
+          />
+          <Carousel.Caption>
+            <h3>Tigre</h3>
+            <p>Le plus grand félin</p>
+          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     </div>
